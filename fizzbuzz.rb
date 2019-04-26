9f9d29edc92209efdbda3124b7e68d9c90ffa88a
Travis Exline <t.exline22@gmail.com>
2019-04-26 15:32:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(numb)
-  if numb % 3 == 0
+  if (numb % 3 == 0) && (numb % 5 == 0)
     return "Fizz"
   elsif numb % 5 == 0
     return "Buzz"
-  elsif (numb % 5 == 0) && (numb % 3 == 0)
+  elsif numb % 3 == 0
     return "FizzBuzz"
   else
     puts "Bad!"
